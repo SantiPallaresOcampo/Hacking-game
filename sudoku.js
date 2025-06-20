57084820c74e9663881d32a34018c012e6848eb1
SantiPallaresOcampo <santiago.pallares@estudiantat.upc.edu>
2025-06-20 11:48:18 +0200

    feat: :sparkles: acabado, falta revisar

--- sudoku.js
+++ sudoku.js
@@ -1,43 +1,28 @@
 // Sudoku resuelto (solución final)
 const solution = [
-  [5,3,4 ,6,7,8 ,9,1,2],
-
-  [6,7,2 ,1,9,5 ,3,4,8],
-  
-  [1,9,8 ,3,4,2 ,5,6,7],
-  
-  [8,5,9 ,7,6,1 ,4,2,3],
-  
-  [4,2,6 ,8,5,3 ,7,9,1],
-  
-  [7,1,3 ,9,2,4 ,8,5,6],
-  
-  [9,6,1 ,5,3,7 ,2,8,4],
-  
-  [2,8,7 ,4,1,9 ,6,3,5],
-  
-  [3,4,5 ,2,8,6 ,1,7,9]
+  [2, 8, 6, 3, 9, 7, 5, 4, 1],
+  [3, 4, 9, 5, 6, 1, 8, 7, 2],
+  [7, 1, 5, 4, 8, 2, 3, 6, 9],
+  [4, 3, 7, 8, 1, 6, 2, 9, 5],
+  [1, 6, 8, 2, 5, 9, 4, 3, 7],
+  [9, 5, 2, 7, 4, 3, 1, 8, 6],
+  [5, 7, 4, 9, 2, 8, 6, 1, 3],
+  [8, 9, 1, 6, 3, 5, 7, 2, 4],
+  [6, 2, 3, 1, 7, 4, 9, 5, 8]
 ];
 
 // Celdas fijas del puzzle inicial (ejemplo: solo algunas posiciones)
 const initialPuzzle = [
-  [null,3,4 ,6,7,8 ,9,1,2],
-
-  [6,7,2 ,1,9,5 ,3,4,8],
   
-  [1,9,8 ,3,4,2 ,5,6,7],
-  
-  [8,5,9 ,7,6,1 ,4,2,3],
-  
-  [4,2,6 ,8,5,3 ,7,9,1],
-  
-  [7,1,3 ,9,2,4 ,8,5,6],
-  
-  [9,6,1 ,5,3,7 ,2,8,4],
-  
-  [2,8,7 ,4,1,9 ,6,3,5],
-  
-  [3,4,5 ,2,8,6 ,1,7,9]
+  [null, 8, null, 3, null, null, null, 4, null],
+  [null, 4, 9, null, 6, null, 8, 7, null],
+  [7, null, null, null, null, 2, 3, 6, null],
+  [4, null, null, 8, null, null, null, null, null],
+  [null, 6, null, 2, 5, null, 4, 3, null],
+  [null, 5, null, null, 4, null, null, 8, null],
+  [null, null, 4, null, null, null, null, null, null],
+  [8, 9, null, null, 3, 5, null, 2, null],
+  [null, 2, null, null, 7, null, null, 5, null]
 ];
 
 function createSudokuGrid() {
